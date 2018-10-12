1773d09bee3bc44bad5c8dd19675d3afa0c4418e
HuntsmanRoym <grins008@morris.umn.edu>
2018-10-11 19:12:44 -0500

    Rewrite file_disemvowel and everything works except for reading from and printing to standard input/output

--- file_disemvowel/file_disemvowel.c
+++ file_disemvowel/file_disemvowel.c
@@ -1,12 +1,14 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-#include "disemvowel.h"
 
-// Determines if a character is a vowel or not
-bool isVowel(char letter) {
-  // Look for capital and lowercase vowels
-  switch(letter) {
+#include <stdbool.h>
+
+#define BUF_SIZE 1024
+
+bool is_vowel(char c) { 
+//	printf("Entered is_vowel\n");
+    switch(c) {
     // All vowels
     case 'A':
     case 'E':
@@ -25,75 +27,86 @@ bool isVowel(char letter) {
     default:
       return false;
       break;
-  }
+  } 
 }
 
-//Counts the number of vowels in the string
-int countVowel(char *str)
-{
-  int count = 0;
-  for(unsigned int i = 0; i < strlen(str); i++)
-  {
-    if(isVowel(str[i]))
-    {
-        count++;
-    }
-  }
-  return count;
-}
+int copy_non_vowels(int num_chars, char* in_buf, char* out_buf) {
+//	printf("Entered copy_non_vowels\n");
+	int out_buf_index = 0;
 
-char *disemvowel(char *str, int lengthOfString)
-{
-	//COMPILING SHORTCUT
-	// g++ -Wall -g -o disemvowel_test disemvowel_test.cpp disemvowel.c -lgtest
-	// g++ -Wall -g -o main main.c disemvowel.c
-	// valgrind --leak-check=full ./disemvowel_test
-	// valgrind --leak-check=full ./main < sample_input.txt
-
-	char* nullTerminatedString = (char* )calloc(sizeof(char), lengthOfString+1);
-	strcpy(nullTerminatedString, str);
-	nullTerminatedString[lengthOfString+1] = '\0';
-
-
-	//Store length of string
-	int old_string_length = strlen(nullTerminatedString);
-	
-	//Test if string contains content
-	if(old_string_length > 0)
-	{
-		//Determine amount of memory to allocate
-		int old_string_vowels = countVowel(nullTerminatedString);
-		int new_string_length = old_string_length - old_string_vowels;
-		char *result = (char*) calloc(new_string_length + 1, sizeof(char)); //Null terminator is NOT included in the size of a string.
-
-		//Create pointer so we can 'append' letters to the end of our string
-		int resultIndex = 0;
-
-		//Start looking through all the letters of the old string to determine what characters we keep for the new string
-		for (int i = 0; i < old_string_length; i++)
-		{
-			//Check to see if it is NOT a vowel
-			if(!isVowel(nullTerminatedString[i]))
-			{
-				//If it isn't, add it to the next spot in our string
-				result[resultIndex] = nullTerminatedString[i];
-				resultIndex++;
-			}
+	for( int i = 0; i < num_chars; i++) {
+		if(!is_vowel(in_buf[i])){
+				out_buf[out_buf_index] = in_buf[i];
+				out_buf_index++;
 		}
-		
-		//All characters added, now finally add null terminator
-		result[new_string_length] = '\0';
-		return result; 
+	}
+	
+	return out_buf_index;
+}
+
+void disemvowel(FILE* inputFile, FILE* outputFile) { 
+    
+//	printf("Entered disemvowel\n");
+	//fopen(inputFile, "r");
+	//fopen(outputFile, "w");
+
+	char* in_buf = (char*)calloc(BUF_SIZE, sizeof(char));
+	char* out_buf = (char*) calloc(BUF_SIZE, sizeof(char));
+	
+	int resultOfRead = fread(in_buf, sizeof(char), BUF_SIZE, inputFile);
+
+	while(resultOfRead != 0){
+//		printf("Entered while loop\n");
+		int numNonVowels = copy_non_vowels(resultOfRead, in_buf, out_buf);
+		fwrite(out_buf, sizeof(char), numNonVowels, outputFile);
+		resultOfRead = fread(in_buf, sizeof(char), BUF_SIZE, inputFile);
 	}
 
-	//String is empty, do nothing
-	else
-	{
-		
-		return nullTerminatedString;
-	}
+	fclose(inputFile);
+	fclose(outputFile);
+	free(in_buf);
+	free(out_buf);
+	/*
+     * Copy all the non-vowels from inputFile to outputFile.
+     * Create input and output buffers, and use fread() to repeatedly read
+     * in a buffer of data, copy the non-vowels to the output buffer, and
+     * use fwrite to write that out. 
+     */
+}
 
-	free(nullTerminatedString);
+int main(int argc, char *argv[]) { 
+    FILE *inputFile; 
+    FILE *outputFile;
 
+    if(argc == 1) {
+	   // printf("Setting up input output files\n");
+	    char *line;
+	    size_t size;
+
+	    size = 100;
+	    line = (char*) malloc(size + 1);
+
+	    while(getline(&line, &size, stdin) > 0){
+	    inputFile = stdin;
+	    outputFile = stdout;
+	    disemvowel(inputFile, outputFile);
+	    }
+	    free(line);
+    }
+
+    if(argc == 2) {
+	    inputFile = fopen(argv[1], "r");
+	    outputFile = stdout;
+    }
+
+    if(argc == 3) {
+	    inputFile = fopen(argv[1], "r");
+	    outputFile = fopen(argv[2], "w");
+    }
+
+  //  printf("Calling disemvowel\n");
+    disemvowel(inputFile, outputFile);
+
+    return 0; 
 }
 
